99083450daf3e749ac0e8573c727c070faa0ecab
Rafael Lins <rslinsx@gmail.com>
2023-10-26 20:34:13 -0300

    Enviando dados para mongodb

--- back/express-node/app.js
+++ back/express-node/app.js
@@ -16,8 +16,13 @@ const app = express();
     res.send('Somente um teste');
   });
 
+  app.get('/mensagens', (req, res)=>{
+    res.send('Essa será a página onde vou printar mensagens mongodb');
+  })
+
 
 
 app.listen(8081, ()=>{
     console.log('Servidor rodando na porta 8081');
 });
+
--- back/express-node/mongodb.js
+++ back/express-node/mongodb.js
@@ -1,9 +1,38 @@
 const mongoose = require('mongoose');
 
-const mongoUrl = 'mongodb://127.0.0.1:27017/';
+const mongoUrl = 'mongodb://127.0.0.1:27017/chatmongodb';
 
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
     console.log('Conectado com sucesso ao mongodb');
 }).catch((err)=>{
     console.log('Erro ao conectar ao mongodb: '+ err);
 });
+
+const mensagensSchema = new mongoose.Schema({
+    conteudo: String
+}, {
+    versionKey: false
+});
+
+const Mensagem = mongoose.model('Mensagens', mensagensSchema);
+
+const newMessage = new Mensagem ({
+    conteudo: 'Esse é um pequeno teste'
+});
+
+// newMessage.save().then(()=>{
+//     console.log('Mensagem enviada com sucesso!');
+// }).catch((err)=>{
+//     console.error('deu esse erro aqui, pai vei: '+err);
+// });
+
+Mensagem.find({}).then((data) => {
+    data.forEach((documento) => {
+      console.log(documento.conteudo);
+    });
+  }).catch((err) => {
+    console.log(err);
+  });
+
+
+
